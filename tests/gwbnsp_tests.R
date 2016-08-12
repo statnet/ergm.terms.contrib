140d2690a86e4d4f3a4bc3f2d1c1eb9ab3e1b095
Skye Bender-deMoll <skyebend@skyeome.net>
2016-08-12 12:36:53 -0700

    test formatting for CRAN checks

--- tests/gwbnsp_tests.R
+++ tests/gwbnsp_tests.R
@@ -5,7 +5,7 @@ require(ergm.terms.contrib)
 bel <- data.frame(actor = c("a","a","a","b","b","b","b","c","c","c","c"), 
                   event = c("e1","e2","e3","e3","e1","e4","e5","e3","e1","e4","e6"))
 
-bnet <- network(bel,bipartite=3,directed=F)
+bnet <- network(bel,bipartite=3,directed=FALSE)
 
 
 # plot for visualy checking the debugging
@@ -16,22 +16,22 @@ bnet <- network(bel,bipartite=3,directed=F)
 test_that('gwbnsp terms', {
 
   # how many actor dyads have ANY shared partners?
-  expect_equal(as.vector( summary(bnet ~ gwb1nsp(0,T)) ), 3)
+  expect_equal(as.vector( summary(bnet ~ gwb1nsp(0,TRUE)) ), 3)
   
   # what is the TOTAL number of shared partners of all actor dyads?
-  expect_equal(as.vector( summary(bnet ~ gwb1nsp(100,T)) ), 7) 
+  expect_equal(as.vector( summary(bnet ~ gwb1nsp(100,TRUE)) ), 7) 
   
   # how many event dyads have ANY shared partners?
-  expect_equal(as.vector( summary(bnet ~ gwb2nsp(0,T)) ), 11)  
+  expect_equal(as.vector( summary(bnet ~ gwb2nsp(0,TRUE)) ), 11)  
   
   # what is the TOTAL number of shared partners of all event dyads?
-  expect_equal(as.vector( summary(bnet ~ gwb2nsp(100,T)) ) , 15) 
+  expect_equal(as.vector( summary(bnet ~ gwb2nsp(100,TRUE)) ) , 15) 
   
   # non-extreme alphas weight each additional shared partner less than the one before
-  expect_equal(as.vector( summary(bnet ~ gwb1nsp(.5,T)) ) , 4.335226, tolerance=1e-5) 
+  expect_equal(as.vector( summary(bnet ~ gwb1nsp(.5,TRUE)) ) , 4.335226, tolerance=1e-5) 
   
   # same as above but for 2nd mode
-  expect_equal(as.vector( summary(bnet ~ gwb2nsp(.5,T)) ), 12.33523 , tolerance=1e-5)
+  expect_equal(as.vector( summary(bnet ~ gwb2nsp(.5,TRUE)) ), 12.33523 , tolerance=1e-5)
 })
 
 
